db6c68fc44066e6ed97961a1c4f34b5b1fa41e0c
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-04-14 22:31:10 -0300

    choree: Add mirageJS to simulate API behavior

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createServer } from 'miragejs'
 
 import App from './views/app'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-)
+createServer({
+  environment: 'development',
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return [
+        { id: 1, title: 'Transaction 1', amount: 100, type: 'deposit', category: 'food', createdAt: new Date() },
+      ]
+    })
+  },
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
