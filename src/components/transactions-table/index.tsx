96f47dcc03ab19f9c369a6a239159a55c5be6959
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-04-22 21:44:51 -0300

    feat: Add integration to table

--- src/components/transactions-table/index.tsx
+++ src/components/transactions-table/index.tsx
@@ -1,33 +1,57 @@
-import React from 'react'
-import classesNames from 'classnames'
+import React, { useEffect, useState } from 'react'
 
+import { api } from '_services/requests'
 import styles from './styles.css'
 
-const TransactionsTable = () => (
-  <table className={styles.table}>
-    <thead className={styles.thead}>
-      <tr>
-        <th>Titulo</th>
-        <th>Valor</th>
-        <th>Categoria</th>
-        <th>Data</th>
-      </tr>
-    </thead>
-    <tbody className={styles.tbody}>
-      <tr>
-        <td>Almoço</td>
-        <td className={classesNames({[styles.deposit]: true})}>R$100.00</td>
-        <td>Restaurante</td>
-        <td>10/10/2018</td>
-      </tr>
-      <tr>
-        <td>Almoço</td>
-        <td className={classesNames({[styles.withdraw]: true})}>R$100.00</td>
-        <td>Restaurante</td>
-        <td>10/10/2018</td>
-      </tr>
-    </tbody>
-  </table>
-)
+interface Transaction {
+  id: number
+  title: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
+}
+
+const TransactionsTable = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    api
+      .get('/transactions')
+      .then((response: any) => setTransactions(response.data.transactions))
+  }, [])
+
+  return (
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr>
+          <th>Titulo</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {transactions.map((transaction) => (
+          <tr key={transaction.id}>
+            <td>{transaction.title}</td>
+            <td className={styles[transaction.type]}>
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(transaction.amount)}
+            </td>
+            <td>{transaction.category}</td>
+            <td>
+              {new Intl.DateTimeFormat('pt-BR').format(
+                new Date(transaction.createdAt)
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
 export default TransactionsTable
--- src/index.tsx
+++ src/index.tsx
@@ -7,17 +7,31 @@ import App from './views/app'
 createServer({
   environment: 'development',
   models: {
-    transactions: Model,
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 100,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date('2021-03-15 08:00:00'),
+        },
+      ],
+    })
   },
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions')
+      return this.schema.all('transaction')
     })
     this.post('/transaction', (schema, request) => {
       const data = JSON.parse(request.requestBody)
-      return data
+      return schema.create('transaction', data)
     })
   },
 })
