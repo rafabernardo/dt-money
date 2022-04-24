0172ceb739ad38106254e533a9e86dab0f7c594f
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-04-24 19:32:38 -0300

    chore: Add custom media queries to postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,16 @@ const postCssNested = require('postcss-nested')
 const autoprefixer = require('autoprefixer')
 const postCssNormalize = require('postcss-normalize')
 const postCssReset = require('postcss-css-reset')
+const postCssCustomMedia = require('postcss-custom-media')
 
 module.exports = {
-  plugins: [postCssNested, autoprefixer(), postCssNormalize(), postCssReset()],
+  plugins: [
+    postCssCustomMedia({
+      importFrom: './src/config/custom-media-queries.js',
+    }),
+    postCssNested,
+    autoprefixer(),
+    postCssReset(),
+    postCssNormalize(),
+  ],
 }
---
+++ src/config/custom-media-queries.js
@@ -0,0 +1,7 @@
+module.exports = {
+  customMedia: {
+    '--sm-viewport': '(min-width:160px) and (max-width:480px)',
+    '--md-viewport': '(min-width:481px) and (max-width:1024px)',
+    '--lg-viewport': '(min-width:1025px)',
+  },
+}
