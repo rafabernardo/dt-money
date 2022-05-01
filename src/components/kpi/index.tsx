a5533bfbe2b53960aa36a4204e78550634db4107
Rafaela Rodrigues <rafaelarodrigues@jungledevs.com>
2022-05-01 19:21:40 -0300

    feat: Mobile view for kpi

--- src/components/kpi/index.tsx
+++ src/components/kpi/index.tsx
@@ -2,42 +2,69 @@ import React, { useMemo } from 'react'
 import classesNames from 'classnames'
 import { PulseLoader } from 'react-spinners'
 
-import { TRANSACTION_TYPES_LABEL } from '_utils/constants'
+import { TRANSACTION_TYPES_LABEL, TRANSACTION_TYPES } from '_utils/constants'
 import { useWindowSize } from '_hooks/use-Window-size'
 import { IKpiProps } from '_types/kpi'
 
 import styles from './styles.css'
 
 const Kpi = ({ value, icon, type, createdAt, isLoading }: IKpiProps) => {
-  console.log('🚀 ~ file: index.tsx ~ line 12 ~ Kpi ~ createdAt', createdAt)
+  console.log('🚀 ~ file: index.tsx ~ line 12 ~ Kpi ~ type', type)
   const { isDesktop } = useWindowSize()
 
+  const statusBackgroundColor = useMemo(() => {
+    if (type === TRANSACTION_TYPES.TOTAL && value > 0) {
+      return '#33cc95'
+    }
+    if (type === TRANSACTION_TYPES.TOTAL && value < 0) {
+      return '#e62e4d'
+    }
+    return 'white'
+  }, [type, value])
+
   return (
-    <div className={styles['kpi-container']}>
+    <div
+      className={styles['kpi-container']}
+      style={{ background: statusBackgroundColor }}
+    >
       {isLoading ? (
         <div className={styles.loading}>
           <PulseLoader color="var(--green)" size={10} />
         </div>
       ) : (
         <>
-          <h2 className={styles.title}>{TRANSACTION_TYPES_LABEL[type]}</h2>
+          <h2
+            className={classesNames(styles.title, {
+              [styles.total]: type === TRANSACTION_TYPES.TOTAL,
+            })}
+          >
+            {TRANSACTION_TYPES_LABEL[type]}
+          </h2>
           <svg viewBox={icon.viewBox} className={styles.icon}>
             <use xlinkHref={`#${icon.id}`} />
           </svg>
-          <p className={styles.text}>
-            {new Intl.NumberFormat('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            }).format(value)}
-          </p>
-          {!isDesktop && (
-            <p className={styles['small-text']}>
-              Última entrada dia
-              {createdAt
-                ? new Intl.DateTimeFormat('pt-BR').format(new Date(createdAt))
-                : ''}
+          <div className={styles.content}>
+            <p
+              className={classesNames(styles.text, {
+                [styles.total]: type === TRANSACTION_TYPES.TOTAL,
+              })}
+            >
+              {new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(value)}
             </p>
-          )}
+            {!isDesktop && createdAt && (
+              <p
+                className={classesNames(styles['small-text'], {
+                  [styles.total]: type === TRANSACTION_TYPES.TOTAL,
+                })}
+              >
+                Última entrada dia
+                {new Intl.DateTimeFormat('pt-BR').format(new Date(createdAt))}
+              </p>
+            )}
+          </div>
         </>
       )}
     </div>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,11 +2,13 @@ export const MOBILE = 480
 export const TABLET = 1024
 
 export const TRANSACTION_TYPES = {
-  WITHDRAW: 'withdraw',
   DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+  TOTAL: 'total',
 }
 
 export const TRANSACTION_TYPES_LABEL = {
   [TRANSACTION_TYPES.WITHDRAW]: 'Saídas',
   [TRANSACTION_TYPES.DEPOSIT]: 'Entradas',
+  [TRANSACTION_TYPES.TOTAL]: 'Total',
 }
